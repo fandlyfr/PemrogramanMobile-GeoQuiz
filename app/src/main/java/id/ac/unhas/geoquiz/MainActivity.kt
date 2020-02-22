d700e4912cecc85a15612a3ae834028cfb918e59
MuhammadFandlyFadlurachman <fandlyfr@gmail.com>
2020-02-22 21:14:53 +0800

    test

--- app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
@@ -2,6 +2,8 @@ package id.ac.unhas.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -16,15 +18,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton:Button
     private lateinit var falseButton:Button
 
+    private val TAG = "Main Activity";
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreate(Bundle?)called")
 
 
         //declare btn and textview
         nextButton = findViewById(R.id.next_btn)
-        previousButton = findViewById(R.id.previous_btn)
+//        previousButton = findViewById(R.id.previous_btn)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton = findViewById(R.id.true_btn)
         falseButton = findViewById(R.id.false_btn)
@@ -43,16 +48,16 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        previousButton.setOnClickListener{
-            currentIndex = (currentIndex-1)%questionBank.size
-            if (currentIndex < 0){
-                Toast.makeText(this,R.string.gk_ada_prev,Toast.LENGTH_SHORT).show()
-                currentIndex = 0
-            }else {
-                val questionTextResId = questionBank[currentIndex].textResId
-                questionTextView.setText(questionTextResId)
-            }
-        }
+//        previousButton.setOnClickListener{
+//            currentIndex = (currentIndex-1)%questionBank.size
+//            if (currentIndex < 0){
+//                Toast.makeText(this,R.string.gk_ada_prev,Toast.LENGTH_SHORT).show()
+//                currentIndex = 0
+//            }else {
+//                val questionTextResId = questionBank[currentIndex].textResId
+//                questionTextView.setText(questionTextResId)
+//            }
+//        }
 
 
         trueButton.setOnClickListener {
@@ -62,6 +67,31 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener {
             checkAnswer(false)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
 
@@ -84,7 +114,9 @@ class MainActivity : AppCompatActivity() {
         }else{
             R.string.wrong_toast
         }
-        Toast.makeText(this,messageId,Toast.LENGTH_SHORT).show()
+        val toast = Toast.makeText(this,messageId,Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP or Gravity.CENTER_VERTICAL,0,0)
+        toast.show()
     }
 }
 
